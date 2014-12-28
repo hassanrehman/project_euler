736dd13e039cd29fbf7c06836f9ee9a4e9cf1f4d
hassan abdul rehman <hassan.rehman@gmail.com>
2014-12-28 18:20:46 +0500

    coin sums finally optimized

--- problem031.rb
+++ problem031.rb
@@ -1,3 +1,5 @@
+require "./utility.rb"
+
 =begin
 In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
 
@@ -8,5 +10,53 @@ It is possible to make £2 in the following way:
 How many different ways can £2 be made using any number of coins?
 =end
 
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
+class P031
 
+  COINS = [200, 100, 50, 20, 10, 5, 2, 1]
+
+  def initialize
+    puts process
+  end
+
+  def process
+    iterations = 0
+    count = 0
+    (0..(200/COINS[0])).to_a.each do |i0|
+      break if value([i0, 0, 0, 0, 0, 0, 0, 0]) > 200
+      (0..(200/COINS[1])).to_a.each do |i1|
+        break if value([i0, i1, 0, 0, 0, 0, 0, 0]) > 200
+        (0..(200/COINS[2])).to_a.each do |i2|
+          break if value([i0, i1, i2, 0, 0, 0, 0, 0]) > 200
+          (0..(200/COINS[3])).to_a.each do |i3|
+            break if value([i0, i1, i2, i3, 0, 0, 0, 0]) > 200
+            (0..(200/COINS[4])).to_a.each do |i4|
+              break if value([i0, i1, i2, i3, i4, 0, 0, 0]) > 200
+              (0..(200/COINS[5])).to_a.each do |i5|
+                break if value([i0, i1, i2, i3, i4, i5, 0, 0]) > 200
+                (0..(200/COINS[6])).to_a.each do |i6|
+                  iterations+=1 
+                  indexes = [i0, i1, i2, i3, i4, i5, i6, 0]
+                  sum = value(indexes)
+                  break if sum > 200
+
+                  count += 1 if sum <= 200
+                end  #i6
+              end  #i5
+            end  #i4
+          end  #i3
+        end  #i2
+      end  #i1
+    end  #i0
+    #puts "iterations: #{iterations}"
+    puts count
+  end
+
+  def value(indexes)
+    sum = indexes.each_with_index.map{|v, i| COINS[i]*v }.sum
+    #puts "#{indexes.inspect} - #{sum}"
+    return sum
+  end
+
+end
+
+P031.new
