a9110532ecf98002f32b033af89a5ded72018054
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2016-09-24 23:48:03 +0100

    206

--- problem206.rb
+++ problem206.rb
@@ -1,25 +1,30 @@
-start_value = 101010103
-end_value = 141421354
+require './utility'
 
-def test(num)
-	str = num.to_s
-	str[-1] == 57 and str[-3] == 56 and str[-5] == 55 and str[-7] == 54 and str[-9] == 53 and str[-11] == 52 and str[-13] == 51 and str[-15] == 50
+#1_2_3_4_5_6_7_8_9_0
+
+#if r is the solution to 1_2_3_4_5_6_7_8_9
+#then solution = r*10
+
+max = 19293949596979899
+max_sq = Math.sqrt(max).ceil
+
+min = 10203040506070809
+min_sq = Math.sqrt(min).ceil + 1    #checked in console until i got in the end
+
+#to get 9 in the end we need 3 or 7 in the end of the number
+#try 23*23 and 57*57 ..  the first digit is 9 .. 
+#so starting from the top .. we come down
+#reducing 4 if it ends with 7 or 6 if it ends with 3
+
+is_in_form = Proc.new do |n|
+    n.to_s.split("").in_groups_of(2).map{|k, v| k }.join == "123456789"
+  end
+
+n = max_sq
+while n > min_sq do 
+  break if is_in_form.call( n**2 )
+  n -= ( n % 10 == 7 ? 4 : 6 )
 end
 
-four = true
-i = start_value
-while i < end_value
-	sq = i*i
-	if test(sq)
-	  puts "#{i} : #{sq}"
-	  sleep(0.5)
-	end
-	
-	if four
-	  i += 4
-	  four = false
-	else
-	  i += 6
-	  four = true
-	end
-end
+puts n*10
+
