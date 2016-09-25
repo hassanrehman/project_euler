c37182de3024acdece7950a6bcade263560d6d5f
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2016-09-25 17:33:14 +0100

    076

--- problem076.rb
+++ problem076.rb
@@ -1,10 +1,42 @@
-target = 100
 
-ways = [1]+[0]*target
-for i in 1...100
-  for j in i..target
-    ways[j] += ways[j-i]
-  end
+
+#https://en.wikipedia.org/wiki/Partition_(number_theory)
+
+def g(k)
+  @@_gk ||= {}
+  @@_gk[k] ||= ( (k * (3*k - 1)) /2 )
 end
 
-puts ways[target]
+def sign(k)
+  @@_signs ||= {}
+  @@_signs[k] ||= ((-1)**(k-1)).to_i
+end
+
+def p(n)
+
+  @@_pn ||= {}
+  return @@_pn[n] if @@_pn[n]
+
+  if n < 0
+    sum = 0
+  elsif n < 1
+    sum = 1
+  else
+    #many of these are > n ...  if this isnt efficient enough .. we can reduce these by a LOT
+    #to save recursions .. but they'd all give 0 from memory since p(n) remembers previous numbers
+    #and negatives are static returns ..  
+    ks = (n/2.0).ceil.times.flat_map{|i| [i + 1, -i - 1] }
+    sum = ks.map{|k| sign(k) * p( n - g(k) ) }.inject(&:+)
+    
+  end
+  @@_pn[n] = sum
+  return sum
+end
+
+
+puts p(100) - 1
+
+
+
+
+
