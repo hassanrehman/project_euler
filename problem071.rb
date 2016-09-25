5f3509c883b2d670b0743eb9d69a1f5df9088920
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2016-09-25 01:32:11 +0100

    071

--- problem071.rb
+++ problem071.rb
@@ -1,50 +1,15 @@
 
 
-def limit
-  @@_limit ||= (3 / 7.0)
-end
+def get_result(d)
+  max = (d*3 / 7.0).ceil    #nearest to be greater than 3/7 value
 
-def limit_fraction
-  @@_limit_fraction ||= Rational(3, 7)
-end
-
-def get_range(d)
-  min = d/3
-  max = d/2
-
-  tolerance = 1   #how much to loop downwards
-
-  iterations = 1
-  loop do 
-
-    mid = (min + max) / 2
-    r = mid / d.to_f 
-
-    if r > limit
-      max = mid
-    else r < limit
-      min = mid
-    end
-
-    break if max - min <= tolerance
-
-    iterations += 1
+  n = max - 1
+  loop do
+    #if n / d < 3 / 7 .. then n is the answer
+    return n if n*7 < d*3
+    n -= 1
 
   end
-
-  # puts "d: #{d}, iterations: #{iterations}"
-  return min, max
-end
-
-def get_result(d)
-  min, max = get_range(d)
-
-  #with tolerance = 1 .. there would be only one of these two candidates to be less than 3/7
-
-  #if max / d < 3 / 7 then max .. etc
-  return max if max * 7 < 3 * d
-  return min if min * 7 < 3 * d
-  return min - 1
 
 end
 
@@ -52,10 +17,6 @@ denominator = 2
 numerator = get_result(denominator)
 
 (3..1_000_000).each do |d|
-  if d % 10_000 == 0
-    puts d
-  end
-
   new_denominator = d
   new_numerator = get_result(d)
 
