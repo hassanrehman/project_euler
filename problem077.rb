5c15e7ed235682ce091adbaf62833978654f9715
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2018-04-23 20:44:05 +0100

    77

--- problem077.rb
+++ problem077.rb
@@ -1,18 +1,22 @@
-primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79]
+require 'prime'
+primes = Prime.each(100).to_a
 
+
+#for every new prime introduced .. the number of summations is
+#the breakdown of two previously already calculated smaller denominations (i)
 target = 2
 loop do
   ways = [1] + [0]*target
   for i in primes
-	#puts i
     for j in i..target
       ways[j] += ways[j-i]
-	  #puts ways.inspect
-	end
+	    # puts "i: #{i}, j: #{j} - #{ways.inspect}"
+	  end
   end
+
   
   break if ways[target] > 5000
   target += 1
 end
 
-puts "Answer to PE77 = #{target}"
+puts target
