4f3e0641b050d1e92c1232f32cdd316f8c6dae20
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2018-04-23 20:24:54 +0100

    75

--- problem075.rb
+++ problem075.rb
@@ -9,28 +9,25 @@ def gcd(a, b)
     t = b
     b = a % b
     a = t
-    end
+  end
   return a
 end
-def triple(m, n)
-  a = 2*m*n
-  b = (m+n)*(m-n)
-  c = m*m+n*n
-  [a, b.abs, c].sort
+
+def triple_sum(m, n)
+  2*m*n + ( (m+n)*(m-n) ).abs + (m*m+n*n)
 end
 
 
 MAXSUM = 1500000
 sqrt_maxsum = Math.sqrt(MAXSUM)
-sums = []
+sums = Hash.new{|h, k| h[k] = 0 }
 (1..sqrt_maxsum).step(2) do |m|
   (2..sqrt_maxsum).step(2) do |n|
-    if gcd(m, n) == 1   #by principle .. 
-      sum = triple(m, n).sum
+    if gcd(m, n) == 1   #by principle .. to only get primitive
+      sum = triple_sum(m, n)
 
       #a multiple of a triple is also a triple .. count
       (sum..MAXSUM).step(sum) do |s|
-        sums[s] ||= 0
         sums[s] += 1
       end
     end
@@ -38,4 +35,4 @@ sums = []
 end
 
 #now count the ones that have just one triple
-puts sums.count(1)
+puts sums.select{|k, v| v == 1 }.count
